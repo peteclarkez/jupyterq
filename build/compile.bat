099cbc9d73d7423445a93e11bc51fb2ad8489a2b
James Hanna <jhanna@kx.com>
2018-06-16 12:57:19 +0100

    always download k.h and q.lib

--- build/compile.bat
+++ build/compile.bat
@@ -1,2 +1,8 @@
 cd %~dp0\..
-cl /LD /Felib\w64\jupyterq.dll src\lib\q.lib /DKXVER=3 /Isrc\include src\c\jupyterq.c
+mkdir w64
+mkdir src\lib
+mkdir src\include
+curl -fsSL -o src\include\k.h https://github.com/KxSystems/kdb/raw/master/c/c/k.h
+curl -fsSL -o lib\w64\q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+cl /LD /Few64\jupyterq.dll src\lib\q.lib /DKXVER=3 /Isrc\include src\c\jupyterq.c
