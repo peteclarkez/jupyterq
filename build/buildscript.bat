1f6be4bb4afd18d0fe48c53061a3004dfb8ab516
James Hanna <jhanna@kx.com>
2018-06-17 18:06:55 +0100

    move everything to build script

---
+++ build/buildscript.bat
@@ -0,0 +1,23 @@
+@echo OFF
+build\compile.bat || goto :error
+
+if not defined QLIC_KC (
+ goto :nokdb
+)
+
+build\getkdb.bat || goto :error
+set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
+mkdir embedpy 
+cd embedpy
+echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
+cd ..
+echo p)print('embedpy runs') | q -q || goto :error
+pip install -r requirements.txt || goto :error
+install.bat || goto :error
+
+:error
+echo failed with error %errorLevel%
+exit /b %errorLevel%
+
+:nokdb
+exit /b 0
