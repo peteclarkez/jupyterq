73128c8d3a8a542dcc2c9589eee9464c02b501f5
James Hanna <jhanna@kx.com>
2018-06-17 19:22:02 +0100

    call

--- build/buildscript.bat
+++ build/buildscript.bat
@@ -1,15 +1,11 @@
-echo compiling
-
-build\compile.bat || goto :error
-
-echo here
+call "build\compile.bat" || goto :error
 
 if not defined QLIC_KC (
  goto :nokdb
 )
 
 @echo OFF
-build\getkdb.bat || goto :error
+call "build\getkdb.bat" || goto :error
 set PATH=C:\Miniconda3-x64;C:\Miniconda3-x64\Scripts;%PATH%
 mkdir embedpy 
 cd embedpy
@@ -17,14 +13,12 @@ echo getembedpy"latest" | q ..\build\getembedpy.q -q || goto :error
 cd ..
 echo p)print('embedpy runs') | q -q || goto :error
 pip install -r requirements.txt || goto :error
-install.bat || goto :error
+call "install.bat" || goto :error
 
 :error
-@echo ON
 echo failed with error %errorLevel%
 exit /b %errorLevel%
 
 :nokdb
-@echo ON
 echo no kdb
 exit /b 0
