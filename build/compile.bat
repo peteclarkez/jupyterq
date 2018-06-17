1cd3d6193e0a99ef48cf84523fbdc2b2f1842462
James Hanna <jhanna@kx.com>
2018-06-17 19:02:48 +0100

    debug

--- build/compile.bat
+++ build/compile.bat
@@ -6,3 +6,4 @@ curl -fsSL -o src\include\k.h https://github.com/KxSystems/kdb/raw/master/c/c/k.
 curl -fsSL -o src\lib\q.lib https://github.com/KxSystems/kdb/raw/master/w64/q.lib
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 cl /LD /Few64\jupyterq.dll src\lib\q.lib /DKXVER=3 /Isrc\include src\c\jupyterq.c
+exit /b 0
