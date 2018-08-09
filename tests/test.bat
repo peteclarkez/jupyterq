96ac13de30c573b4ae6333c7eedd1f18533cd090
James Hanna <jhanna@kx.com>
2018-08-09 10:27:35 +0100

    debug

--- tests/test.bat
+++ tests/test.bat
@@ -4,6 +4,8 @@ if defined QLIC_KC (
         echo CD:%CD%
         echo 0N!.z.K | q -q
         dir q.k
+        where jupyter
+        del C:\projects\jupyterq\q\w64\q.exe
         pip -q install -r tests\requirements.txt
 	jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
 	jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples/html_display.ipynb
