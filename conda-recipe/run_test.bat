76681f37b9c875a583ee3eba61d3769116651417
James Hanna <jhanna@kx.com>
2018-08-09 10:32:36 +0100

    debug

--- conda-recipe/run_test.bat
+++ conda-recipe/run_test.bat
@@ -1,6 +1,7 @@
 if not defined QLIC (
  goto :nokdb
 )
+del C:\projects\jupyterq\q\w64\q.exe :: TODO remove debug
 call tests\test.bat || goto :error
 exit /b 0
 
--- tests/test.bat
+++ tests/test.bat
@@ -5,7 +5,6 @@ if defined QLIC_KC (
         echo 0N!.z.K | q -q
         dir q.k
         where jupyter
-        del C:\projects\jupyterq\q\w64\q.exe
         pip -q install -r tests\requirements.txt
 	jupyter nbconvert --allow-errors --to notebook --execute --ExecutePreprocessor.timeout=60 --output tests/test.out kdb+Notebooks.ipynb
 	jupyter nbconvert --to notebook --execute --ExecutePreprocessor.timeout=60 --output test.out examples/html_display.ipynb
